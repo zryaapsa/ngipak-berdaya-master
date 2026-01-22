2f42c1bc449612ad34e15eff241c7888e1d4eeb4
zryaapsa <pandu.surya0412@gmail.com>
2026-01-22 21:11:03 +0700

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
+import AppRoutes from "./app/routes";
 
-import { useState, useEffect } from 'react'
-import { supabase } from '../utils/supabase'
-
-function Page() {
-  const [todos, setTodos] = useState([])
-
-  useEffect(() => {
-    function getTodos() {
-      const { data: todos } = await supabase.from('todos').select()
-
-      if (todos.length > 1) {
-        setTodos(todos)
-      }
-    }
-
-    getTodos()
-  }, [])
-
-  return (
-    <div>
-      {todos.map((todo) => (
-        <li key={todo}>{todo}</li>
-      ))}
-    </div>
-  )
+export default function App() {
+  return <AppRoutes />;
 }
-export default Page
